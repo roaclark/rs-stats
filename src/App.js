c07b2d853295283a1b2277e1c386f3b0dd22bcc6
Rachel Rosalia <rosalia@stripe.com>
2020-12-06 14:39:51 -0800

    Server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  React.useEffect(() => fetch('/foo').then(c => c.text()).then(console.log))
   return (
     <div className="App">
       <header className="App-header">
