dd8b4964d8c96c05ebcdf4046cae569304d788b9
Rachel Rosalia <rachel@rosalia.me>
2020-12-21 23:44:53 -0800

    Overview icons and styling

--- src/Summary.js
+++ src/Summary.js
@@ -1,6 +1,36 @@
 import styled from "styled-components";
 import _ from "lodash";
 
+const icons = {
+  attack: "https://oldschool.runescape.wiki/images/f/fe/Attack_icon.png",
+  hitpoints: "https://oldschool.runescape.wiki/images/9/96/Hitpoints_icon.png",
+  mining: "https://oldschool.runescape.wiki/images/4/4a/Mining_icon.png",
+  strength: "https://oldschool.runescape.wiki/images/1/1b/Strength_icon.png",
+  agility: "https://oldschool.runescape.wiki/images/8/86/Agility_icon.png",
+  smithing: "https://oldschool.runescape.wiki/images/d/dd/Smithing_icon.png",
+  defence: "https://oldschool.runescape.wiki/images/b/b7/Defence_icon.png",
+  herblore: "https://oldschool.runescape.wiki/images/0/03/Herblore_icon.png",
+  fishing: "https://oldschool.runescape.wiki/images/3/3b/Fishing_icon.png",
+  ranged: "https://oldschool.runescape.wiki/images/1/19/Ranged_icon.png",
+  thieving: "https://oldschool.runescape.wiki/images/4/4a/Thieving_icon.png",
+  cooking: "https://oldschool.runescape.wiki/images/d/dc/Cooking_icon.png",
+  prayer: "https://oldschool.runescape.wiki/images/f/f2/Prayer_icon.png",
+  crafting: "https://oldschool.runescape.wiki/images/c/cf/Crafting_icon.png",
+  firemaking:
+    "https://oldschool.runescape.wiki/images/9/9b/Firemaking_icon.png",
+  magic: "https://oldschool.runescape.wiki/images/5/5c/Magic_icon.png",
+  fletching: "https://oldschool.runescape.wiki/images/9/93/Fletching_icon.png",
+  woodcutting:
+    "https://oldschool.runescape.wiki/images/f/f4/Woodcutting_icon.png",
+  runecrafting:
+    "https://oldschool.runescape.wiki/images/6/63/Runecraft_icon.png",
+  slayer: "https://oldschool.runescape.wiki/images/2/28/Slayer_icon.png",
+  farming: "https://oldschool.runescape.wiki/images/f/fc/Farming_icon.png",
+  construction:
+    "https://oldschool.runescape.wiki/images/f/f6/Construction_icon.png",
+  hunter: "https://oldschool.runescape.wiki/images/d/dd/Hunter_icon.png",
+};
+
 const statsGrid = [
   ["attack", "hitpoints", "mining"],
   ["strength", "agility", "smithing"],
@@ -15,13 +45,14 @@ const statsGrid = [
 const Container = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
-  grid-column-gap: 3em;
+  grid-column-gap: 2em;
   grid-row-gap: 10px;
 `;
 
 const StyledStat = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
   color: ${(props) => (props.filtered ? "#555" : "white")};
 `;
 
@@ -30,16 +61,27 @@ const SkillName = styled.span`
   margin-right: 1em;
 `;
 
+const SkillImage = styled.img`
+  margin-right: 0.75em;
+  width: 28px;
+  height: 28px;
+  background: rgba(255, 255, 255, 0.75);
+  border: 1px solid white;
+  border-radius: 17px;
+  padding: 3px;
+`;
+
 const SkillLevel = styled.div``;
 
 const Stat = ({ stat, experience, getLevel, filtered }) => {
   if (!experience) {
     return "Failure" + stat;
   }
+  const icon = icons[stat];
   const statLevel = getLevel(experience);
   return (
     <StyledStat filtered={filtered}>
-      <SkillName>{stat}</SkillName>
+      {icon ? <SkillImage src={icon} /> : <SkillName>{stat}</SkillName>}
       <SkillLevel filtered={filtered}>{statLevel}</SkillLevel>
     </StyledStat>
   );
