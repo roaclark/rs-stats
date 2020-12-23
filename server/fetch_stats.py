ad300a59713158e2fa7b594d705f609b9e27614d
Rachel Rosalia <rachel@rosalia.me>
2020-12-22 23:14:44 -0800

    Fix stats dump

--- server/fetch_stats.py
+++ server/fetch_stats.py
@@ -15,10 +15,10 @@ def fetch_stats(old_stats=None, persist=False):
     stats = {k: max(stats[k], old_stats[k]) for k in stats if k in old_stats}
   if persist:
     with open('./data/stats.csv','rw') as f:
-      data = 'skill,exp\n' + '\n'.join([','.join(k, stats[k]) for k in stats])
+      data = 'skill,exp\n' + '\n'.join([','.join([k, str(stats[k])]) for k in stats])
   return stats
 
 if __name__ == '__main__':
   stats = fetch_stats()
   for k in stats:
-    print(k + ':', stats[k])
+    print(k + ',' + str(stats[k]))
